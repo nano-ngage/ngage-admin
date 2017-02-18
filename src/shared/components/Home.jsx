81178f9fd25c9f0fc590dacae5482247ef9ed7c0
Jordan Medina <jordanwmedina@gmail.com>
2017-02-17 16:23:44 -0800

    (feat) Add placeholder components App and Home

---
+++ src/shared/components/Home.jsx
@@ -0,0 +1,10 @@
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
+}
