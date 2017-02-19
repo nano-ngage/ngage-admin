be25e702ac2edd026b05c1454a535708e11891ef
Jordan Medina <jordanwmedina@gmail.com>
2017-02-18 16:25:59 -0800

    (refactor) Add routing for server-side rendering
    - Ability to server static files at /dist/*
    - All other routes (for now) get matched against components
    - Promisify all components (for the potential async data they need)
    - Prepare the data and html from the components & async data
    - Use the handlebars template to render

--- src/server/server.js
+++ src/server/server.js
@@ -1,46 +1,59 @@
 import express from 'express';
 import path from 'path';
 import handlebars from 'express-handlebars';
-import Promise form 'bluebird';
+import Promise from 'bluebird';
 
 // Inferno Imports
 import Inferno from 'inferno';
-import { renderToString } from 'inferno-server';
-import { RouterContext, match } from 'inferno-router';
-import { createMemoryHistory } from 'history';
-import createRoutes from '../shared/routes.jsx';
+import { match } from 'inferno-router';
+import routes from '../shared/routes.jsx';
+import { extractComponents, mapComponentsToPromises, prepareData, render } from './lib/helpers';
 
 const app = express();
 
 // Template Engine with handlebars
-app.set('views', path.resolve('views'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', handlebars({
   extname: 'hbs',
   defaultLayout: 'main.hbs',
-  layoutsDir: path.resolve('views', 'layouts')
+  layoutsDir: path.join(__dirname, 'views', 'layouts')
 }));
 app.set('view engine', 'hbs');
 
+const router = express.Router();
+
+// Static Assets Here -- NOTE: Express Static doesn't work for this
+router.get('/dist/*', function (req, res){
+  res.sendFile(path.join(__dirname, '/../', req.url));
+})
+
 // Universal Routing with Inferno-Router
-app.get('*', (req, res) => {
-  const history = createMemoryHistory();
-  const routes = createRoutes(history);
+router.get('*', (req, res) => {
+  const renderProps = match(routes, req.originalUrl)
+  if (renderProps.redirect) {
+    return res.redirect(renderProps.redirect);
+  }
 
-  match({routes: routes, location: req.location}, (err, redirLoc, rProps) => {
-    if (redirLoc) {
-      res.redirect(301, redirLoc.pathname + redirLoc.search);
-    } else if (err) {
-      res.send(500, err.message);
-    } else if (rProps === null) {
-      res.send(404, 'Not found')   // <-- later replace with 404 route/component
-    } else {
+  const unfilteredComponents = extractComponents(renderProps.matched.props);
+  const { promises, components } = mapComponentsToPromises(unfilteredComponents, renderProps.matched.params);
 
-      // TODO: Make all components into promises (the ones that do DB fetching)
-      // Then once all of the data has loaded do res.render passing in:
-      // - content (html from renderToString)
-      // - context (the data from the DB) -- to be set on the Window initially
-    }
-  })
+  Promise.all(promises)
+    .then((values) => {
+      const data = prepareData(values, components);
+      const html = render(renderProps, data);
+
+      res.render('index', {
+        content: html,
+        context: JSON.stringify(data)
+      });
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
-export default app;
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
