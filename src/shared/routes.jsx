52f18b67874ffafb5a350573293751b2f7a29eaf
Jordan Medina <jordanwmedina@gmail.com>
2017-02-18 16:13:07 -0800

    (refactor) Update to return routes (not func)

--- src/shared/routes.jsx
+++ src/shared/routes.jsx
@@ -2,13 +2,11 @@ import Inferno from 'inferno';
 import { Router, Route, IndexRoute } from 'inferno-router';
 
 // Import Components Here
-import App from './containers/index.jsx';
+import App from './components/index.jsx';
 import Home from './components/Home.jsx';
 
-export default history => (
-  <Router history={history}>
-    <Route path="/" component={ App }>
-      <IndexRoute component={ Home } />
-    </Route>
-  </Router>
+export default (
+  <Route path="/" component={ App }>
+    <IndexRoute component={ Home } />
+  </Route>
 )
