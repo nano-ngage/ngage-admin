dbb058df03ae0f8166277110570288ca6d3e1dcf
Jordan Medina <jordanwmedina@gmail.com>
2017-02-18 16:15:27 -0800

    (refactor) Update to only run babel and server.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,3 @@
 require('babel-register');
 require('babel-polyfill');
-
-var server = require('./server');
-var PORT = 3000;
-
-server.list(PORT, function(err) {
-  console.log('Web server listening at port: ' PORT);
-});
+require('./server');
