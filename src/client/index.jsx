36b2258b897b2604ea02662f51c2ef4f11dc75ac
Sunnie Mao <mao.sunnie@gmail.com>
2017-02-20 15:21:38 -0800

    (feat) render css

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,6 +3,7 @@ import Inferno from 'inferno';
 import { Router } from 'inferno-router';
 import { createBrowserHistory } from 'history';
 import routes from '../shared/routes.jsx';
+import {ContextWrapper} from '../shared/components/common/ContextWrapper.jsx'
 
 const history = createBrowserHistory();
 
