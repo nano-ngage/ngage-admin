c986b2bb3cd6c93ce2c48b55a486d78151050f3e
Sunnie Mao <mao.sunnie@gmail.com>
2017-02-20 15:22:37 -0800

    (feat) add Navigation and revise routes

--- src/shared/components/index.jsx
+++ src/shared/components/index.jsx
@@ -1,13 +1,29 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import Nav from './Nav.jsx';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userID: 0
+    };
+  }
+
+  componentDidMount() {
+    //  FETCH USER INFORMATION
+    // fetch('/item-names').then((data) => {
+    //   this.setState({
+    //     data
+    //   });
+    // });
+  }
+
   render() {
     return (
-      <div id="app">
-        <h1>ngage</h1>
-        <hr />
-        {this.props.children}
+      <div  id="app">
+      <Nav />
+      {this.props.children}
       </div>
     );
   }
