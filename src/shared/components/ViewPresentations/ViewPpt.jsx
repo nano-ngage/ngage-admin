9a73c09d060de9780601b59105bd335ff05d30bd
Sunnie Mao <mao.sunnie@gmail.com>
2017-02-21 17:36:38 -0800

    (fix) seperate create ppt and edit ppt

--- src/shared/components/ViewPresentations/ViewPpt.jsx
+++ src/shared/components/ViewPresentations/ViewPpt.jsx
@@ -13,7 +13,7 @@ class ViewPpt extends Component {
         <div className="viewPpt">
           <span className="title">PID: {this.props.ppt.presentationID} (todo: change to title)</span>
             <span className="actions"><span className="action">Start</span>&nbsp;|&nbsp;
-            <Link to={"/create/" + this.props.ppt.presentationID} className="action">Edit</Link>&nbsp;|&nbsp;
+            <Link to={"/edit/" + this.props.ppt.presentationID} className="action">Edit</Link>&nbsp;|&nbsp;
             <span className="action" onClick={() => {this.props.delete(this.props.ppt.presentationID)}}>Delete</span></span>
         </div>
       </div>
--- src/shared/routes.jsx
+++ src/shared/routes.jsx
@@ -6,13 +6,15 @@ import App from './components/index.jsx';
 import Home from './components/Home.jsx';
 
 import CreatePresentation from './components/CreatePresentation/CreatePresentation.jsx';
+import EditPresentation from './components/CreatePresentation/EditPresentation.jsx';
 import ViewPresentations from './components/ViewPresentations/ViewPresentations.jsx';
 
 
 export default (
   <Route path="/" component={ App }>
     <IndexRoute component={ Home } />
-    <Route path="create/:id" component={ CreatePresentation } />
+    <Route path="create" component={ CreatePresentation } />
     <Route path="view" component={ ViewPresentations } />
+    <Route path="edit/:id" component={ EditPresentation } />
   </Route>
 )
