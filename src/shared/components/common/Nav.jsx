b7b0fc017fdac6d813ae7e5eb2a84fa59db72506
Jordan Medina <jordanwmedina@gmail.com>
2017-02-24 15:22:16 -0800

    (feat) Update Nav based on auth status

--- src/shared/components/common/Nav.jsx
+++ src/shared/components/common/Nav.jsx
@@ -2,6 +2,16 @@ import Inferno from 'inferno';
 import { Link } from 'inferno-router';
 
 export default function Nav(props) {
+  let logger;
+  if (props.user) {
+    logger = [<li><Link to="/" className='navLink' >Home</Link></li>,
+      <li><Link to="view" className='navLink'>View</Link></li>,
+      <li><Link to="create"  className='navLink'>Create</Link></li>,
+      <li><Link to="logout" className='navLink'>Logout</Link></li>
+    ]
+  } else {
+    logger = (<Link to="login" className='navLink'>Login</Link>)
+  }
   return (
     <div className='navbg'>
       <div className='navContainer'>
@@ -11,10 +21,7 @@ export default function Nav(props) {
         </header>
         <nav>
           <ul className='navul'>
-            <li><Link to="/" className='navLink' >Home</Link></li>
-            <li><Link to="/view" className='navLink'>View</Link></li>
-            <li><Link to="/create"  className='navLink'>Create</Link></li>
-            <li><Link to="/logout" className='navLink'>Logout</Link></li>
+            {logger}
           </ul>
         </nav>
       </div>
