7cb8be1295d8ea8fa330ad902270f9f8401a0847
Saivickna Raveendran <saivickna.r@gmail.com>
2017-02-25 13:41:47 -0800

    (fix) Updated deployment scripts to reference environment variables

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo "pulling latest version of the code"
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull nanongage/ngage-admin:latest'
 
 echo "starting the new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --link ngage-db:ngagedb -e DBIP="ngagedb" --name ngage-admin -p 3001:3001 nanongage/ngage-admin:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always -e DBIP="'$DBIP'" -e SESSION_SECRET="'$SESSION_SECRET'" -e AUTH0_CLIENT_SECRET="'$AUTH0_CLIENT_SECRET'" -e AUTH0_CLIENT_ID="'$AUTH0_CLIENT_ID'" -e AUTH0_DOMAIN="'$AUTH0_DOMAIN'" --name ngage-admin -p 3001:3001 nanongage/ngage-admin:latest'
 
 echo "success!"
 
