f47c710fe8b90ad0c5edca5eda12df979fb1fc10
Jordan Medina <jordanwmedina@gmail.com>
2017-02-27 14:31:54 -0800

    (refactor) Update Login.jsx for client-side rendering

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -7,38 +7,16 @@ export default class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      auth: new AuthService(AUTH0_CLIENT_ID, AUTH0_DOMAIN, this.handleLogin.bind(this))
+      auth: new AuthService(AUTH0_CLIENT_ID, AUTH0_DOMAIN, this.props.handleUser)
     }
   }
 
-  handleLogin(user) {
-    window.localStorage.setItem('user', JSON.stringify(user));
-    this.props.handleUser(user)
-
-    // Setting up this reference so it can be used in
-    // different "this" context below
-    const handleUser = this.props.handleUser;
-
-    // This fetch request is for the purpose of validating
-    // a user login in the event someone tries to do a direct
-    // post request to our /initsession with fake auth_id & user_id
-    // (there's no way to get a specific users's user_id from our DB)
-    fetch('http://104.131.147.199:3001/initsession', {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-      mode: 'cors',
-      body: JSON.stringify(user)
-    })
-    .then(res => res.json())
-    .then(verifiedUser => {
-      handleUser(verifiedUser);
-    })
-    .catch(err => { console.error('oops, something went wrong', err); });
-
-  }
-
   componentDidMount() {
-    this.state.auth.login();
+    if (!this.state.auth.loggedIn()) {
+      this.state.auth.show();
+    } else {
+      window.browserHistory.push('/');
+    }
   }
 
   render() {
