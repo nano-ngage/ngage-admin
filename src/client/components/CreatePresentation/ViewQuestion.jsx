75a3bf01885a9ca5021e37c981bd81e349de5f5a
Jordan Medina <jordanwmedina@gmail.com>
2017-02-27 18:59:47 -0800

    (refactor) Hardcode DBIP & DBPORT for ViewQuestion.jsx

--- src/client/components/CreatePresentation/ViewQuestion.jsx
+++ src/client/components/CreatePresentation/ViewQuestion.jsx
@@ -12,13 +12,12 @@ function getAs(qid) {
 }
 
 class ViewQuestion extends Component {
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props);
     this.state = {
-      items: (context.data[ViewQuestion.NAME] || []),
       answers: []
     };
-    dbURL = `http://${DBIP}:${DBPORT}`;
+    dbURL = `http://104.131.147.199:5000`;
 
     this.viewAnswers = this.viewAnswers.bind(this);
   }
