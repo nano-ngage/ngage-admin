7d9812d13c4713d4dfc82a89393ffe4e956ccc92
Sunnie Mao <mao.sunnie@gmail.com>
2017-03-03 19:25:55 -0800

    (feat) fetch groups, create new groupID, and delete groups

--- src/client/components/Groups/CreateGroup.jsx
+++ src/client/components/Groups/CreateGroup.jsx
@@ -1,191 +1,151 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 // import AddQuestion from './AddQuestion.jsx';
-import ViewGroup from './ViewGroup.jsx';
+// import ViewGroup from './ViewGroup.jsx';
 
 // create edit Q as seperate everything...
 var dbURL = '';
-function initPid(userID) {
-  return fetch(dbURL + '/pByU',{
+function initGid(userID) {
+  return fetch(dbURL + '/g',{
     method: 'POST',
     mode: 'CORS',
     headers: {'Content-Type': 'application/JSON'},
-    body: JSON.stringify({'userID': userID, 'title': 'New Presentation'})
+    body: JSON.stringify({'userID': userID, 'name': 'New Group'})
     }).then(data => data.json());
 }
 
-function getQs(pid) {
-  return fetch(dbURL + '/qByP/'+pid,{
-    method: 'GET',
-    mode: 'CORS',
-    headers: {'Content-Type': 'application/JSON'},
-    }).then(data => data.json()).catch();
-}
-
-function updateTitle(pid, title) {
-  return fetch(dbURL + '/p/' + pid,{
+function updateName(gid, name) {
+  return fetch(dbURL + '/g/' + gid,{
     method: 'PUT',
     mode: 'CORS',
     headers: {'Content-Type': 'application/JSON'},
-    body: JSON.stringify({'title': title})
+    body: JSON.stringify({'name': name})
     }).then(data => data.json());
 }
 
-function postQ(pid, type, question) {
-  return fetch(dbURL + '/qByP',{
-    method: 'POST',
-    mode: 'CORS',
-    headers: {'Content-Type': 'application/JSON'},
-    body: JSON.stringify({'presentationID': pid, 'type': type, 'question': question})
-    }).then(data => data.json());
-}
+// function getUsers(pid) {
+//   return fetch(dbURL + '/qByP/'+pid,{
+//     method: 'GET',
+//     mode: 'CORS',
+//     headers: {'Content-Type': 'application/JSON'},
+//     }).then(data => data.json()).catch();
+// }
 
-function postAnswers(qid, answers) {
-  return fetch(dbURL + '/aByQs',{
-    method: 'POST',
-    mode: 'CORS',
-    headers: {'Content-Type': 'application/JSON'},
-    body: JSON.stringify({'questionID': qid, 'answers': answers})
-    }).then(data => data.json());
-}
+// function postQ(pid, type, question) {
+//   return fetch(dbURL + '/qByP',{
+//     method: 'POST',
+//     mode: 'CORS',
+//     headers: {'Content-Type': 'application/JSON'},
+//     body: JSON.stringify({'presentationID': pid, 'type': type, 'question': question})
+//     }).then(data => data.json());
+// }
 
-function editQ(qid) {
-  return fetch(dbURL + '/q/' + qid,{
-    method: 'PUT',
-    mode: 'CORS',
-    headers: {'Content-Type': 'application/JSON'},
-    body: JSON.stringify({'UPDATEHERE': 'FOR PID, QID, QUESTION, TYPE'})
-    }).then(data => data.json());
-  // add body with corrent format above
-}
+// function postAnswers(qid, answers) {
+//   return fetch(dbURL + '/aByQs',{
+//     method: 'POST',
+//     mode: 'CORS',
+//     headers: {'Content-Type': 'application/JSON'},
+//     body: JSON.stringify({'questionID': qid, 'answers': answers})
+//     }).then(data => data.json());
+// }
 
-function deleteQ(qid) {
-  return fetch(dbURL + '/q/'+ qid,{
-    method: 'DELETE',
-    mode: 'CORS',
-    headers: {'Content-Type': 'application/JSON'}
-    }).then(data => data.json()).catch();
-}
+// function editQ(qid) {
+//   return fetch(dbURL + '/q/' + qid,{
+//     method: 'PUT',
+//     mode: 'CORS',
+//     headers: {'Content-Type': 'application/JSON'},
+//     body: JSON.stringify({'UPDATEHERE': 'FOR PID, QID, QUESTION, TYPE'})
+//     }).then(data => data.json());
+//   // add body with corrent format above
+// }
+
+// function deleteQ(qid) {
+//   return fetch(dbURL + '/q/'+ qid,{
+//     method: 'DELETE',
+//     mode: 'CORS',
+//     headers: {'Content-Type': 'application/JSON'}
+//     }).then(data => data.json()).catch();
+// }
 
 class CreateGroup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      presentationID: 0,
-      title: '',
-      type: 1,
-      typeDescription: 'Multiple Choice',
-      qid: 0,
-      questions: []
+      groupID: 0,
+      name: ''
+      // qid: 0,
+      // questions: []
     };
     dbURL = `http://104.131.147.199:5000`;
 
-    this.handleTitle = this.handleTitle.bind(this);
-    this.handleType = this.handleType.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.deleteQuestion = this.deleteQuestion.bind(this);
-    this.addToViewQuestions = this.addToViewQuestions.bind(this);
+    this.handleName = this.handleName.bind(this);
+    // this.handleType = this.handleType.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.deleteQuestion = this.deleteQuestion.bind(this);
+    // this.addToViewQuestions = this.addToViewQuestions.bind(this);
   }
 
   componentDidMount() {
-    if (this.props.params.id > 0) {
-      this.setState({presentationID: this.props.params.id});
-      this.setState({title: this.props.params.title});
-      getQs(this.props.params.id).then(data => {
-        this.setState({questions: data});
-      }).catch(error => {});
-    } else if (this.props.user) {
-      initPid(this.props.user.userID)
-        .then(data => {this.setState({presentationID: data.presentationID})});
+    // if (this.props.params.id > 0) {
+    //   this.setState({groupID: this.props.params.id});
+    //   this.setState({title: this.props.params.title});
+    //   getQs(this.props.params.id).then(data => {
+    //     this.setState({questions: data});
+    //   }).catch(error => {});
+    // } else 
+    if (this.props.user) {
+      initGid(this.props.user.userID)
+        .then(data => {this.setState({groupID: data.groupID})});
     }
   }
 
-  handleTitle(e) {
+  handleName(e) {
     e.preventDefault();
-    this.setState({title: e.target.value});
-  }
-
-  handleType(e, type) {
-    e.preventDefault();
-    this.setState({type: type});
-    var types = {
-      1: 'Multiple Choice',
-      2: 'Free Response',
-      3: '🙁 😐 🙂',
-      4: '👍 👎',
-      5: '⭐ ⭐ ⭐'
-    };
-    this.setState({typeDescription: types[type]});
+    this.setState({name: e.target.value});
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    updateTitle(this.state.presentationID, this.state.title);
+    updateName(this.state.groupID, this.state.name);
   }
 
-  deleteQuestion(qid) {
-    var questions = [];
-    this.state.questions.forEach((question, index) => {
-      if (question.questionID !== qid) {
-        questions.push(question);
-      }
-    });
-    this.setState({questions: questions});
-    deleteQ(qid);
-  }
+  // deleteQuestion(qid) {
+  //   var questions = [];
+  //   this.state.questions.forEach((question, index) => {
+  //     if (question.questionID !== qid) {
+  //       questions.push(question);
+  //     }
+  //   });
+  //   this.setState({questions: questions});
+  //   deleteQ(qid);
+  // }
 
-  addToViewQuestions(newQuestion, answers) {
-    postQ(this.state.presentationID, this.state.type, newQuestion)
-      .then(data => {
-        var questions = this.state.questions;
-        questions.push({question: newQuestion, questionID: data.questionID});
-        this.setState({questions: questions});
-        postAnswers(data.questionID, answers);
-      }).catch();
-  }
+  // addToViewQuestions(newQuestion, answers) {
+  //   postQ(this.state.presentationID, this.state.type, newQuestion)
+  //     .then(data => {
+  //       var questions = this.state.questions;
+  //       questions.push({question: newQuestion, questionID: data.questionID});
+  //       this.setState({questions: questions});
+  //       postAnswers(data.questionID, answers);
+  //     }).catch();
+  // }
 
   render() {
     return (
       <div className="container">
         <div className="question">
               <div className="submitFlex">
-                <div className="presentationText">Presentation Title<br/></div>
-                <input type="text" className="input" type="text" placeholder="Title goes here" value={this.state.title} onInput={this.handleTitle} autoComplete="off" />
+                <div className="presentationText">Group Name<br/></div>
+                <input type="text" className="input" type="text" placeholder="Group name goes here" value={this.state.name} onInput={this.handleName} autoComplete="off" />
             </div>
         </div>
 
-        {this.state.questions.length > 0 ?
-          (<div className="question">
-            <p className="questionText">Saved Questions<br/></p>
-            {this.state.questions.map((question, index) => {
-                return (
-                  <ViewQuestion question={question} index={index} delete={this.deleteQuestion} />
-                )
-              })
-            }
-            <br />
-          </div>
-          ) : ''
-        }
 
-      <p className="presentation">&nbsp;Question Type: {this.state.typeDescription}</p>
-        <div className="addQuestion">
-          <a href="#" className="ppt" onClick={(e) => {this.handleType(e, 1)}}><p className="option">Multiple Choice</p></a>
-          <a href="#" className="ppt" onClick={(e) => {this.handleType(e, 2)}}><p className="option">Free Response</p></a>
-          <a href="#" className="ppt" onClick={(e) => {this.handleType(e, 3)}}><p className="option2">🙁 😐 🙂</p></a>
-          <a href="#" className="ppt" onClick={(e) => {this.handleType(e, 4)}}><p className="option2">👍 👎</p></a>
-          <a href="#" className="ppt" onClick={(e) => {this.handleType(e, 5)}}><p className="option">⭐ ⭐ ⭐</p></a>
-        </div>
-
-        <AddQuestion presentationID={this.state.presentationID}
-          addToViewQuestions={this.addToViewQuestions}
-          type = {this.state.type}
-          length={this.state.questions.length + 1} />
 
         <div className="submitFlex">
         <form onSubmit={this.handleSubmit}>
           <div className="submitFlexPpt">
-          <button type="submit" className="button">✓ Save Presentation</button>
+          <button type="submit" className="button">✓ Group</button>
           </div>
         </form>
         </div>
