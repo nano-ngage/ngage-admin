55776e43f0b343250ac167c43b2c452d15a6fee0
Saivickna Raveendran <saivickna.r@gmail.com>
2017-03-03 19:18:10 -0800

    (feat) Add environment variables and compression to server and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const webpackValidator = require('webpack-validator');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const getIfUtils = require('webpack-config-utils').getIfUtils;
+var CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = env => {
   const { ifProd, ifNotProd } = getIfUtils(env);
@@ -30,7 +31,22 @@ module.exports = env => {
       ]
     },
     plugins: [
-      new ProgressBarPlugin()
+      new ProgressBarPlugin(),
+      new webpack.DefinePlugin({
+        'DBIP': JSON.stringify(process.env.DBIP || '104.131.147.199'),
+        'DBPORT': JSON.stringify(process.env.DBPORT || 5000),
+        'STATSIP': JSON.stringify(process.env.STATSIP || '104.131.147.199'),
+        'STATSPORT': JSON.stringify(process.env.STATSPORT || 4555),
+        'AUTH0_CLIENT_ID': JSON.stringify(process.env.AUTH0_CLIENT_ID || 'Wnw35lcs32PF5TnkuSu7Aqb3j0sMP0Z4'),
+        'AUTH0_DOMAIN': JSON.stringify(process.env.AUTH0_DOMAIN || 'joraffe.auth0.com'),
+      }),
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+      }),
     ]
   });
 }
